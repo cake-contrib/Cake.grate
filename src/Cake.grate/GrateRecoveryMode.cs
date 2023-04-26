29ff172365d1899d577d846c00cfeded3571a430
Fran Hoey <fran.hoey@intechnica.co.uk>
2023-04-26 15:52:14 +0100

    Initial commit taking the Aliases and Settings from Cake.Common.Tools.Roundhouse

---
+++ src/Cake.grate/GrateRecoveryMode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cake.grate
+{
+    /// <summary>
+    /// Defines the recovery model for SQL Server.
+    /// </summary>
+    public enum GrateRecoveryMode
+    {
+        /// <summary>
+        /// Doesn't change the mode
+        /// </summary>
+        NoChange,
+
+        /// <summary>
+        /// Does not create backup before migration
+        /// </summary>
+        Simple,
+
+        /// <summary>
+        /// Creates log backup before migration
+        /// </summary>
+        Full
+    }
+}
